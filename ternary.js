3d29e9be32f585922966ea89a4ce8be4fbba73ca
Susana Pham <susana.pham@bouvet.no>
2018-06-12 20:14:41 +0200

    New app Ternary

---
+++ ternary.js
@@ -0,0 +1,19 @@
+//Are you legal to drive a car?
+
+// Ternary
+const bedtime = 11;
+
+const time = (bedtime  ? "Time top sleep" : "You can program more")
+
+console.log(time)
+
+
+//if else
+
+// const bedtime = 11;
+
+// if(bedtime) {
+//   console.log("Time top sleep);
+// } else {
+//   console.log("you can program more");
+// }
