9cdc18a5123ad7dd7fe0e603b3ba1ca17bce554b
Susana Pham <susana.pham@bouvet.no>
2018-07-08 21:23:58 +0200

    Added new app for-of.js

--- for-of.js
+++ for-of.js
@@ -3,13 +3,14 @@ let number = [20, 50, 70, 200, 600]
 for (let total of number) {
   let tax = total * .25
   let sum = total - tax
-
+  // If number is under 100, tax is 0
   if (total <= 100) {
     tax = 0;
+    // If number is over 400, tax only 10%
   } if (total >= 400) {
       tax = total * .10
   } else {
-    sum
+    tax
   }
 
   console.log("total is: " + total + "kr")
