0cf99d09ebe8caa988044bd5c5ebb906270d3a91
Susana Pham <susana.pham@bouvet.no>
2018-07-10 13:10:00 +0200

    Fixa for-of.js

--- for-of.js
+++ for-of.js
@@ -1,17 +1,19 @@
 let number = [20, 50, 70, 200, 600]
 
 for (let total of number) {
-  let tax = total * .25
-  let sum = total - tax
+  let tax;
+  let sum;
   // If number is under 100, tax is 0
+
   if (total <= 100) {
     tax = 0;
     // If number is over 400, tax only 10%
-  } if (total >= 400) {
+    } else if (total >= 400) {
       tax = total * .10
   } else {
-    tax
+    tax = total * .25
   }
+  sum = total - tax
 
   console.log("total is: " + total + "kr")
   console.log("tax is: " + tax + "kr")
